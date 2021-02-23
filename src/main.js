7c89fa677069e60cfff6bb6a8f3be7eaec347e3b
fiona <fiona@whispark.com>
2021-02-23 14:45:15 +0800

    feate#main 搭建ssr 导入sync  添加懒加载图片设置webp图片格式

--- src/main.js
+++ src/main.js
@@ -9,14 +9,38 @@
 import Vue from 'vue'
 import App from './App.vue'
 import 'amfe-flexible' // lib-flexible 用于设置 rem 基准值
-import router from './router'
+import { createRouter } from './router'
+import { createStore } from './store'
 import VueLazyLoad from 'vue-lazyload'
-Vue.use(VueLazyLoad)
- 
-
+import { sync } from 'vuex-router-sync'
+// 添加懒加载图片并设置webp图片格式（这种格式目前只能在android下才能使用）
+Vue.use(VueLazyLoad,{
+    loading:require('./assets/imgs/loading.gif'),
+    listenEvents: ['scroll'],
+    filter: {
+        webp(listener, options) {
+          if (!options.supportWebp) return
+          const isCDN = /xiaohuochai.site/
+          if (isCDN.test(listener.src)) {
+            listener.src += '?imageView2/2/format/webp'
+          }
+        }
+    }
+})
 Vue.config.productionTip = false
+export default function createApp() {
+    // 创建 router 和 store 实例
+    const router = createRouter()
+    const store = createStore()
 
-new Vue({
-  router,
-  render: h => h(App),
-}).$mount('#app')
+    // 同步路由状态（route state）到 Store
+    sync(store, router)
+    const app = new Vue({
+        router,
+        store,
+        render: h => h(App),
+    }) 
+    // 暴露 app router和 store
+    return  { app , router , store }
+}
+
