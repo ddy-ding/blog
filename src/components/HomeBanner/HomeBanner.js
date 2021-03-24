456ec4d202865215232a594edff9cedb3e63b797
fiona <fiona@whispark.com>
2021-03-24 15:39:05 +0800

    feat:增加左右按钮图片+修改图片轮播最后一张空白的问题

--- src/components/HomeBanner/HomeBanner.js
+++ src/components/HomeBanner/HomeBanner.js
@@ -1,3 +1,4 @@
+
 export default {
   name:'homeBanner',
   data() {
@@ -34,18 +35,107 @@ export default {
           src: require('@/assets/imgs/react.png'),
           url: ''
         }
-      ]
+      ],
+      isTransitioning: false, // 是否处于动画执行期间
+      isResetIndex: false, // 是否为瞬间重置定位，在watch中初始化，重定位会取消transition动画
+      activeIndex:1, // 默认显示第一张 ===2=== activeIndex对应的操作
+      timer:null, //自动播放的定时器
+      interval: 5000,
+      transitionInterval: 500, // 过渡动画时长
     }
   },
   computed: {
-    // 显示小数点的数量为度之后的图像数量的一半
-    // dotLen() {
-    //   return this.imgDatas / 2
-    // }
+    // 显示小数点的数量
+    dotLen() {
+      return this.len - 2
+    },
+    /**
+     * @description 无缝滚动，在传进来的img数组首部增加末尾元素，尾部追加首元素
+     * @returns
+     */
+    imgsComputed() {
+      const firstImg = this.imgDatas[0];
+      const lastImg = this.imgDatas[this.imgDatas.length - 1]
+      return [lastImg].concat(this.imgDatas, [firstImg])
+    },
+    len() {
+      return this.imgsComputed.length
+    }
+  },
+  mounted() {
+    this.startInterval()
+  },
+  beforeDestroy() {
+    clearInterval(this.timer)
+  },
+  watch: {
+    activeIndex(newIndex, oldIndex) {
+       // ==5== 监听activeIndex的变化，当activeIndex到达边界的时候进行复位
+       // if(是轮播图从最后一张切换到第一张 || 是轮播图从第一张切换到最后一张) 是属于瞬间归位，
+       // 直接return，不执行以后所有函数
+       if((newIndex === 1 && oldIndex === (this.len - 1)) || (oldIndex === 0 && newIndex === (this.len -2))) {
+         this.isResetIndex = true;
+         return
+       }
+       this.isResetIndex = false;
+       this.isTransitioning = true; 
+       setTimeout(() => {
+        // 瞬间归位需要取消transition过渡
+        // 以下两种情况是瞬间归位时activeIndex变化
+        if (this.activeIndex === 0) {
+          this.activeIndex = this.len - 2
+        } else if (this.activeIndex === (this.len - 1)) {
+          this.activeIndex = 1
+        }
+        this.isTransitioning = false // 为false时表示transition过渡结束，可以进行切换轮播
+      }, this.transitionInterval)
+    }
   },
   methods:{
-    hoverFn() {
-      console.log('我是鼠标过度')
-    }
+    /**
+     * @description 鼠标移入暂停移动
+     * @param {string} e
+     * @returns 是否定时
+     */
+    hoverFn(e) {
+      if(e.type === 'mouseenter') {
+         if(this.timer) {
+           clearInterval(this.timer)
+         } else {
+           this.startInterval
+         }
+      }
+    },
+    /**
+     * @description 启动自动轮播
+     * @returns
+     */
+    startInterval() {
+      this.timer = setInterval(() => {
+        this.activeIndex++
+      },this.interval)
+    },
+    /**
+     *@description 小圆点
+     *@param {string} e
+     *@returns
+     */
+     activeDotFn(e) {
+       if(e.target.tagName.toLowerCase() === 'li') {
+         let index = parseInt(e.target.getAttribute('data-dot-index'))
+         this.activeIndex = index
+       }
+     },
+     /**
+      * @description 切换
+      * @param {object} e
+      */
+      switchFn(e) {
+        if(this.isTransitioning) {
+          return 
+        }
+        // ===4===activeIndex操作
+        e.target.className.indexOf('next') !== -1 ? this.activeIndex++ : this.activeIndex --;
+      }
   }
 }
--- src/components/HomeBanner/HomeBanner.vue
+++ src/components/HomeBanner/HomeBanner.vue
@@ -1,22 +1,36 @@
 <template>
   <section class="wrap">
-    <ul class="imgs-container"  @mouseenter="hoverFn">
+    <!-- ===3===对应的activeIndex -->
+    <ul 
+      class="imgs-container" 
+      :style="{width:len*100+'%', transform:`translate(-${activeIndex*100/len}%)`,transition:isResetIndex ? '': `transform ${transitionInterval/1000}s`}"  
+      @mouseenter="hoverFn">
       <li 
-        v-for="(ele, i) in imgDatas" 
+       :style="{width:`calc(100% / ${len})`}"
+        v-for="(ele, i) in imgsComputed" 
         :key="i"
-        class="img-item"
-      >
-        <img :src="ele.src" :alt="ele.name"/>
+        class="img-item">
+        <img 
+          :src="ele.src" 
+          :alt="ele.name" 
+          class="img-style"/>
       </li>
     </ul>
-    <!-- <ul class="dots">
+    <div class="prev">
+      <img src="./img/left.png" alt="" width="30" @click="switchFn">
+    </div>
+    <div class="next">
+      <img src="./img/right.png" alt="" width="30" @click="switchFn">
+    </div>
+    <ul class="dots">
       <li 
-      v-for="(el, i) in dotLen"
+      v-for="(n, i) in dotLen"
       :key="i"
       class="dot" 
+      :class="{'active-dot': activeIndex === n || ( activeIndex===(len-1) && n === 1 )||( activeIndex===0 && n===(len - 2) )}"
+      :data-dot-index="n"
       @click="activeDotFn"></li>
-    </ul> -->
-    
+    </ul>
   </section>
 </template>
  
